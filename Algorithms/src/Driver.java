99ab3573ee75845336de827c63e5e0f0596928a1
Eoin <eoin229@gmail.com>
2022-08-22 18:38:35 +0100

    Quicksort algorithm with a random pivot. O(n logn)

--- Algorithms/src/Driver.java
+++ Algorithms/src/Driver.java
@@ -6,15 +6,20 @@ public class Driver {
 	public static void main(String[] args) {
 		//Create random array
 		Random rand = new Random();
-		int[] numbers = new int[100];
+		int[] numbers = new int[10];
 		for(int i=0; i<numbers.length; i++) {
 			numbers[i] = rand.nextInt(1000);
 		}
 		System.out.println("Before: " + Arrays.toString(numbers));
 		
+		/*
 		MergeSort merge = new MergeSort();
 		merge.sort(numbers);
 		System.out.println("After: " + Arrays.toString(numbers));
+		*/
 		
+		QuickSort quick = new QuickSort();
+		quick.sort(numbers, 0, numbers.length-1);
+		System.out.println("After: " + Arrays.toString(numbers));
 	}
 }
